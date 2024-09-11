2cde00f5269b8914c4c0447f8294a7d777c1c357
vishnu <vishnuprasadvv24@gamil.comgit config --global user.name vishnuprasadvv>
2024-09-11 15:57:43 +0530

    signup ui created

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+    role:{
+        type: String, 
+        default:'user',
+        enum:['user','admin']
+    }
 
 },{timestamps:true})
 
