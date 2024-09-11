5bc828a03d3ab1d2e6990d50d84d83eaa3a58663
vishnu <vishnuprasadvv24@gamil.comgit config --global user.name vishnuprasadvv>
2024-09-11 11:58:15 +0530

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("connected to mongodb successfully")
+}).catch((err)=>{
+    console.log(err);
+})
+ 
 const app = express();
 
 app.listen(3000, ()=>{
